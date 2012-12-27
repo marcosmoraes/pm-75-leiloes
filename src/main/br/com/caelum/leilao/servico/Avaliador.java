134cabf12096616845d12f847e71f2441e6aa77e
marcosmoraes <marcospsmoraes@gmail.com>
2012-12-27 16:00:20 -0200

    Update src/main/br/com/caelum/leilao/servico/Avaliador.java

--- src/main/br/com/caelum/leilao/servico/Avaliador.java
+++ src/main/br/com/caelum/leilao/servico/Avaliador.java
@@ -6,7 +6,7 @@ import br.com.caelum.leilao.dominio.Leilao;
 class Avaliador {   
 
 	private double maiorDeTodos = Double.NEGATIVE_INFINITY; 
-	private double menorDeTodos = Double.POSITIVE_INFINITY;
+	private double menorDeTodos = Dowuble.POSITIVE_INFINITY;
 	private double media = 0;
 
 	public void avalia(Leilao leilao) {
